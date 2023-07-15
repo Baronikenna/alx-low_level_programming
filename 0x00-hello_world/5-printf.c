b4203d3d059c44c285055fa492c5530bd6c56982
ikenna <baronikenna@gmail.com>
2023-07-15 01:59:37 -0700

    g

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this is the main fucntion
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
