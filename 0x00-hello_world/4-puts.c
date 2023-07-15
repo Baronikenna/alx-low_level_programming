547f8a608caa62545674f2aeec5a164c9528fc88
ikenna <baronikenna@gmail.com>
2023-07-15 00:26:20 -0700

    g

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ *main this is the main function
+ *Return - always 0
+ */
+
+int main (void)
+{
+	put("\"Programming is like building a multilingual puzzle);
+	retun;0
+}
