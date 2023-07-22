4f38be8f2a57824cc0b057cc5b96977fbceec85a
ikenna <baronikenna@gmail.com>
2023-07-22 15:40:47 -0700

    h

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,17 +7,18 @@
  */
 int main(void)
 {
-	int limit = 1024;
-	int sum = 0;
+    int limit = 1024;
+    int sum = 0;
+    int i;
 
-	for (int i = 1; i < limit; i++)
-	{
-		if (i % 3 == 0 || i % 5 == 0)
-		{
-			sum += i;
-		}
-	}
+    for (i = 1; i < limit; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+        {
+            sum += i;
+        }
+    }
 
-	printf("The sum of all multiples of 3 or 5 below 1024 is: %d\n", sum);
-	return (0);
+    printf("The sum of all multiples of 3 or 5 below 1024 is: %d\n", sum);
+    return (0);
 }
