98497e2cc3541ea8da42997336764d275444204e
ikenna <baronikenna@gmail.com>
2023-08-28 00:02:58 -0700

    f

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - kuegfufg
+ * @argc: ygyure
+ * @argv: hcfnhuihifg
+ * Return: jnf 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("argv[%d]: %s\n", i, argv[i]);
+	}
+	return (0);
+}
