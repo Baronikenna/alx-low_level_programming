852907f33b6c060ef25f2ac94b5461f7d710c430
ikenna <baronikenna@gmail.com>
2023-08-27 22:59:29 -0700

    g

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - ifgyugfyuqgefgqeqfugu
+ * @argv: bkbfierqgui
+ * @argc: hcbgfqu8gwfgeyfgygeyg
+ * Return: jvfyefgyegqyfveu 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
