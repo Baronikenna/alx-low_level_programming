fd62ba215b89cbaf13675077072bf245ec7e3f25
ikenna <baronikenna@gmail.com>
2023-08-29 00:29:04 -0700

    g

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,68 +1,202 @@
-#include <stdio.h>
+#include "main.h"
+		
 #include <stdlib.h>
-#include <stdbool.h>
+		
+#include <stdio.h>
+		
 
+		
+#define ERR_MSG "Error"
+		
+
+		
 /**
- * isNumeric - Checks if a string is composed of digits only
- * @str: The input string to check
- * Return: true if the string is numeric, false otherwise
+		
+ * is_digit - checks if a string contains a non-digit char
+		
+ * @s: string to be evaluated
+		
+ *
+		
+ * Return: 0 if a non-digit is found, 1 otherwise
+		
  */
-bool isNumeric(char *str)
+		
+int is_digit(char *s)
+		
 {
-	while (*str)
-	{
-		if (*str < '0' || *str > '9')
-		{
-			return (false);
-		}
-		str++;
-	}
-	return (true);
-}
+		
+	int i = 0;
+		
 
+		
+	while (s[i])
+		
+	{
+		
+		if (s[i] < '0' || s[i] > '9')
+		
+			return (0);
+		
+		i++;
+		
+	}
+		
+	return (1);
+		
+}
+		
+
+		
 /**
- * stringToInt - Converts a string to an integer
- * @str: The input string to convert
- * Return: The converted integer value
+		
+ * _strlen - returns the length of a string
+		
+ * @s: string to evaluate
+		
+ *
+		
+ * Return: the length of the string
+		
  */
-int stringToInt(char *str)
+		
+int _strlen(char *s)
+		
 {
-	int result = 0;
+		
+	int i = 0;
+		
 
-	while (*str)
+		
+	while (s[i] != '\0')
+		
 	{
-		result = result * 10 + (*str - '0');
-		str++;
+		
+		i++;
+		
 	}
-	return (result);
+		
+	return (i);
+		
 }
+		
 
+		
 /**
- * main - Entry point of the program
- * @argc: The number of command-line arguments
- * @argv: An array of strings representing the command-line arguments
- * Return: 0 if successful, 98 if there's an error
+		
+ * errors - handles errors for main
+		
  */
+		
+void errors(void)
+		
+{
+		
+	printf("Error\n");
+		
+	exit(98);
+		
+}
+		
+
+		
+/**
+		
+ * main - multiplies two positive numbers
+		
+ * @argc: number of arguments
+		
+ * @argv: array of arguments
+		
+ *
+		
+ * Return: always 0 (Success)
+		
+ */
+		
 int main(int argc, char *argv[])
+		
 {
-	if (argc != 3)
+		
+	char *s1, *s2;
+		
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+		
+
+		
+	s1 = argv[1], s2 = argv[2];
+		
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		
+		errors();
+		
+	len1 = _strlen(s1);
+		
+	len2 = _strlen(s2);
+		
+	len = len1 + len2 + 1;
+		
+	result = malloc(sizeof(int) * len);
+		
+	if (!result)
+		
+		return (1);
+		
+	for (i = 0; i <= len1 + len2; i++)
+		
+		result[i] = 0;
+		
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+		
 	{
-		printf("Error\n");
-		return (98);
+		
+		digit1 = s1[len1] - '0';
+		
+		carry = 0;
+		
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		
+		{
+		
+			digit2 = s2[len2] - '0';
+		
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+		
+			result[len1 + len2 + 1] = carry % 10;
+		
+			carry /= 10;
+		
+		}
+		
+		if (carry > 0)
+		
+			result[len1 + len2 + 1] += carry;
+		
 	}
-
-	if (!isNumeric(argv[1]) || !isNumeric(argv[2]))
+		
+	for (i = 0; i < len - 1; i++)
+		
 	{
-		printf("Error\n");
-		return (98);
+		
+		if (result[i])
+		
+			a = 1;
+		
+		if (a)
+		
+			_putchar(result[i] + '0');
+		
 	}
-
-	int num1 = stringToInt(argv[1]);
-	int num2 = stringToInt(argv[2]);
-
-	int result = num1 * num2;
-
-	printf("%d\n", result);
-
+		
+	if (!a)
+		
+		_putchar('0');
+		
+	_putchar('\n');
+		
+	free(result);
+		
 	return (0);
+		
 }
+
